0f85e76c107c0c05659c2bb027c2e89642609f13
maxbeizer <max.beizer@gmail.com>
2012-11-17 14:55:57 -0600

    add one application feature with steps and fabricator

---
+++ features/step_definitions/application_steps.rb
@@ -0,0 +1,6 @@
+Given "I have started an application already" do
+  Fabrication(:student_application)
+end
+
+Given "I have not started an application already" do
+end
---
+++ spec/fabricators/student_application_fabricator.rb
@@ -0,0 +1,2 @@
+Fabricator(:student_application) do
+end
