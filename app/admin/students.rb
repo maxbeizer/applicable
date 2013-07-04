92aa67b1bb094bc02fca2a346d4bf4da95c6e4d1
Max Beizer <max.beizer@gmail.com>
2013-07-04 14:38:54 -0500

    add basic elements to active admin

--- app/admin/students.rb
+++ app/admin/students.rb
@@ -1,9 +1,33 @@
 ActiveAdmin.register Student do
+  config.per_page = 10
   index do
     column :first_name
     column :last_name
     column :email
-    column :created_at
-    column :program_selection
+    column :updated_at
+    default_actions
   end
+
+  show do |student|
+    attributes_table do
+      row :name
+      row :phone
+      row :email
+      row :date_of_birth
+      row :twitter
+      row :personal_url
+      row :applications_count do
+        student.applications.count
+      end
+
+      row :created_at
+      row :updated_at
+    end
+    active_admin_comments
+  end
+
+  filter :email
+  filter :first_name
+  filter :last_name
+  filter :twitter
 end
