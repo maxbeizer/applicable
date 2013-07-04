9ef4032e1ec349199c84acf4c30220bbed905266
Max Beizer <max.beizer@gmail.com>
2013-07-04 14:48:30 -0500

    remove student index link from admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,7 +3,6 @@ ActiveAdmin.register_page "Dashboard" do
   menu :priority => 1, :label => proc{ I18n.t("active_admin.dashboard") }
 
   content :title => proc{ I18n.t("active_admin.dashboard") } do
-    link_to 'See All Students', '/students'
 
     # Here is an example of a simple dashboard with columns and panels.
     #
